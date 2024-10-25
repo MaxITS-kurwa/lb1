d129afde77c0d334f8177bbd330074ce04d9742f
MaxITS-kurwa <ncfu.maso@gmail.com>
2024-10-25 12:31:30 +0300

    внесение иcправлений в код

--- кросспл Макс.py
+++ кросспл Макс.py
@@ -2,7 +2,7 @@ import random
 
 numbers = [random.randint(1, 100) for _ in range(10)]
 
-sorted_numbejffffffffffffffs = sorted(numbers)
+sorted_numbers = sorted(numbers)
 
 print("Начальный список:", numbers)
 print("Готовый список:", sorted_numbers)

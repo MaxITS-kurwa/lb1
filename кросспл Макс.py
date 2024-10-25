903a5288a0896d86346b0a4912f53dbf64939d26
MaxITS-kurwa <ncfu.maso@gmail.com>
2024-10-25 12:25:03 +0300

    изменение 7 строки кода

--- кросспл Макс.py
+++ кросспл Макс.py
@@ -4,5 +4,5 @@ numbers = [random.randint(1, 100) for _ in range(10)]
 
 sorted_numbers = sorted(numbers)
 
-print("Исходный список:", numbers)
+print("Начальный список:", numbers)
 print("Отсортированный список:", sorted_numbers)
